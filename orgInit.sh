552b4469618a5ccff914a7a5ea2034096872a10e
Sumanth Ghanta <92057458+sghanta-sf@users.noreply.github.com>
2024-10-23 12:24:06 -0700

    Update orgInit.sh with cli updates

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sf demoutil org create scratch -f config/project-scratch-def.json -d 1 -s
+sf demoutil org create scratch -f config/project-scratch-def.json -d 1 -s -e escalation.workshop
 sf package install -r -p 04t3k000001yuer --wait 20
 sf shane user psl -n datamask_DataMaskUserPsl -g User -l User
 sf org assign permset -n datamask
